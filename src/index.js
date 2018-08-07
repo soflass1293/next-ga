015f826f8078dad556fad01af3650fbf6a89ff0f
soflass1293 <soflass1293@gmail.com>
2018-08-07 12:25:58 +0100

    Fixed issue "not working in production"

--- src/index.js
+++ src/index.js
@@ -2,36 +2,39 @@ import { Component, createElement } from "react";
 import * as analytics from "./analytics";
 
 function isLocal() {
-  return location.hostname === "localhost";
+    return location.hostname === "localhost";
 }
 
 function isDev() {
-  return process.env.NODE_ENV !== "production";
+    return process.env.NODE_ENV !== "production";
 }
 export default (code, { router }) => Page => {
-  class WithAnalytics extends Component {
-    componentDidMount() {
-      const shouldTrack = isLocal() || isDev()
-      if (!shouldTrack) return;
-      analytics.init(code);
-      analytics.pageview();
+    class WithAnalytics extends Component {
+        componentDidMount() {
+            const shouldTrack = isLocal() || isDev();
+            analytics.init(code);
+            analytics.pageview();
 
-      // listen route changes
-      if (router && router.events && typeof router.events.on === "function") {
-        router.events.on("routeChangeComplete", () => {
-          analytics.pageview();
-        });
-      }
+            // listen route changes
+            if (
+                router &&
+                router.events &&
+                typeof router.events.on === "function"
+            ) {
+                router.events.on("routeChangeComplete", () => {
+                    analytics.pageview();
+                });
+            }
+        }
+
+        render() {
+            return createElement(Page, { ...this.props, analytics });
+        }
     }
 
-    render() {
-      return createElement(Page, { ...this.props, analytics });
+    if (Page.getInitialProps) {
+        WithAnalytics.getInitialProps = Page.getInitialProps;
     }
-  }
 
-  if (Page.getInitialProps) {
-    WithAnalytics.getInitialProps = Page.getInitialProps;
-  }
-
-  return WithAnalytics;
+    return WithAnalytics;
 };
